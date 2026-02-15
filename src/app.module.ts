e0b59fc6fbe2bfb623f541a16b1ed70c41b3452a
HammadZamir <annibaba009@gmail.com>
2026-02-15 04:25:25 +0100

    Add TypeORM integration and user management features

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,28 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import {AnimalsModule} from './animals/animals.module';
+import { AnimalsModule } from './animals/animals.module';
 import { VouchersController } from './vouchers/vouchers.controller';
 import { VouchersService } from './vouchers/vouchers.service';
-import { UsersController } from './users/users.controller';
-import { UsersService } from './users/users.service';
 import { UsersModule } from './users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Users } from './users/users.entity';
 
 @Module({
-  imports: [AnimalsModule, UsersModule],
-  controllers: [AppController, VouchersController, UsersController],
-  providers: [AppService, VouchersService, UsersService],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'root',
+      database: 'test',
+      entities: [Users],
+      synchronize: true,
+    }),
+    AnimalsModule,
+    UsersModule],
+  controllers: [AppController, VouchersController],
+  providers: [AppService, VouchersService],
 })
 export class AppModule {}
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,11 +1,14 @@
 import { Body, Controller, Get } from '@nestjs/common';
 import { Users } from './users.interface';
+import { UsersService } from './users.service';
 
 @Controller('users')
 export class UsersController {
+    constructor( private readonly userService: UsersService){ }
+    
     @Get()
-    getAllUsers(@Body() validateUserDto: Users){
-
+    getAllUsers(@Body() validateUserDto: any){
+        
     }
 
 }
